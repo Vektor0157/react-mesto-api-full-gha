40d7470266caebef54e198082bf800715d067957
Viktoria Ovsyannikova <vichka.ovsyannikova07@gmail.com>
2023-11-15 18:30:29 +0300

    четвёртый коммит

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -19,18 +19,13 @@ const createCard = (req, res, next) => {
 // Обработчик для GET /cards
 const getCards = (req, res, next) => {
   Card.find({})
-    .then((cards) => {
-      if (cards.length === 0) {
-        return res.status(200).send({ message: 'Список карточек пуст' });
-      }
-      return res.status(200).send({ cards });
-    })
+    .then((cards) => res.status(200).send({ data: cards }))
     .catch(next);
 };
 // Обработчик для DELETE /cards/:cardId
 const deleteCard = (req, res, next) => {
   const { cardId } = req.params;
-  Card.findById(cardId)
+  Card.findById({ _id: cardId })
     .then((card) => {
       if (!card) {
         throw new NotFoundError('Карточка по указанному _id не найдена.');
@@ -55,34 +50,34 @@ const deleteCard = (req, res, next) => {
 
 // Обработчик для PUT /cards/:cardId/likes
 const likeCard = (req, res, next) => {
+  const { cardId } = req.params;
   Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $addToSet: { likes: req.user._id } },
+    cardId,
+    { $addToSet: { likes: req.user } },
     { new: true },
   )
     .then((card) => {
       if (card) {
-        res.status(200).send({ data: card });
-      } else {
-        throw new NotFoundError('Карточка с указанным _id не найдена');
+        return res.status(200).send({ data: card });
       }
+      throw new NotFoundError('Карточка с указанным _id не найдена');
     })
     .catch(next);
 };
 
 // Обработчик для DELETE /cards/:cardId/likes
 const dislikeCard = (req, res, next) => {
+  const { cardId } = req.params;
   Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $pull: { likes: req.user._id } },
+    cardId,
+    { $pull: { likes: req.user } },
     { new: true },
   )
     .then((card) => {
       if (card) {
-        res.send({ data: card });
-      } else {
-        throw new NotFoundError('Карточка с указанным _id не найдена');
+        return res.status(200).send({ data: card });
       }
+      throw new NotFoundError('Карточка с указанным _id не найдена');
     })
     .catch(next);
 };
