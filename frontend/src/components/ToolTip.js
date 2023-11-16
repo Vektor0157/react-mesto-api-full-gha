22503d2d94addca7ce67196ed0bce18c9f4fb0ad
Viktoria Ovsyannikova <vichka.ovsyannikova07@gmail.com>
2023-11-17 01:59:31 +0300

    коммит после первого ревью

--- frontend/src/components/ToolTip.js
+++ frontend/src/components/ToolTip.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import fail from '../images/authFail.svg'
+import OK from '../images/authOk.svg'
+
 
 function ToolTip({ name, onSubmit, isOpen, isSuccess, onClose, altClose}) {
 	const title = isSuccess ? "Вы успешно зарегистрировались!" : "Что-то пошло не так! Попробуйте еще раз.";
@@ -8,7 +11,7 @@ function ToolTip({ name, onSubmit, isOpen, isSuccess, onClose, altClose}) {
 			<div className="popup__container" onClick={altClose}>
 				<div className="popup__form" name={`form-edit-${name}`} onSubmit={onSubmit} noValidate>
 					<button className="popup__close" aria-label="close" type="reset" onClick={onClose}/>
-					<img className={`${isSuccess ? `popup__auth` : `popup__auth-fail`}`} alt={title}/>
+					<img className={`${isSuccess ? `popup__auth` : `popup__auth-fail`}`} src={isSuccess ? OK : fail} alt={title}/>
 					<h2 className="popup__title-tooltip">{title}</h2>
 				</div>
 			</div>
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 const apiSettings = {
-	baseUrl: "https://mesto.nomoreparties.co/v1/cohort-65",
+	baseUrl: "https://api.vektor.nomoredomainsmonster.ru",
 	headers: {
 		authorization: "9b20c238-487b-494a-9d41-6bdaba921db5",
 		'Content-Type': 'application/json',
